65352a48b2adb79214fe81c72046d5a9b5e9a9c1
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-23 22:29:49 +0600

    Added test on attributes rendering

---
+++ test/attributes.js
@@ -0,0 +1,15 @@
+/* global describe, it */
+
+var bemjson2html = require('..');
+require('should');
+
+describe('attributes rendering', function () {
+    it('some tricky attributes', function () {
+        var bemjson = { block: 'checkbox', attrs: { type: 'button', disabled: false, value: null } };
+        bemjson2html(bemjson)
+        .should.equal('<div class=\"checkbox\" type=\"button\" disabled=\"false\" value=\"null\"></div>');
+    });
+
+});
+
+
